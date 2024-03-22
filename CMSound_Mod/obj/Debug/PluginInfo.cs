831e63bdaedc3e9fdf333b6809fc61f45dd62de3
CMarink <christianm7765@gmail.com>
2024-03-22 17:33:40 -0400

    Initial Mod Commit To New Repository

---
+++ CMSound_Mod/obj/Debug/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CMSound_Mod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CMSound_Mod";
+        public const string PLUGIN_NAME = "";
+        public const string PLUGIN_VERSION = "";
+    }
+}
---
+++ CMSound_Mod/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CMSound_Mod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CMSound_Mod";
+        public const string PLUGIN_NAME = "CMSound_Mod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}

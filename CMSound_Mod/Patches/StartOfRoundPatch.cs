a631731a3b868f65b40f62d3eefc2621027a7b79
CMarink <christianm7765@gmail.com>
2024-04-03 17:15:36 -0400

    Testing + Documentation Complete
    Added some Documentation to the mod so that other people can learn how to add their own sounds to the game.
    
    I also finished testing the mod to make sure that the Scooby sounds loads on the different sounds and is random.

--- CMSound_Mod/Patches/StartOfRoundPatch.cs
+++ CMSound_Mod/Patches/StartOfRoundPatch.cs
@@ -16,26 +16,43 @@ namespace CMSound_Mod.Patches
         [HarmonyPostfix]
         static void OverrideAudio(StartOfRound __instance)
         {
+            //Creates 15 independent random numbers using the C# random class
+            //A new random variable is created each time because it appearently has to do with the systam clock and creating a new one each time ensures that it is fully random
             Random random_number_generator = new Random();
-
             int random_number_1 = random_number_generator.Next(0, 2);
-            int random_number_2 = random_number_generator.Next(0, 2);
-            int random_number_3 = random_number_generator.Next(0, 2);
-            int random_number_4 = random_number_generator.Next(0, 2);
-            int random_number_5 = random_number_generator.Next(0, 2);
-            int random_number_6 = random_number_generator.Next(0, 2);
-            int random_number_7 = random_number_generator.Next(0, 2);
-            int random_number_8 = random_number_generator.Next(0, 2);
-            int random_number_9 = random_number_generator.Next(0, 2);
-            int random_number_10 = random_number_generator.Next(0, 2);
-            int random_number_11 = random_number_generator.Next(0, 2);
-            int random_number_12 = random_number_generator.Next(0, 2);
-            int random_number_13 = random_number_generator.Next(0, 2);
-            int random_number_14 = random_number_generator.Next(0, 2);
-            int random_number_15 = random_number_generator.Next(0, 2);
+            Random random_number_generator_2 = new Random();
+            int random_number_2 = random_number_generator_2.Next(0, 2);
+            Random random_number_generator_3 = new Random();
+            int random_number_3 = random_number_generator_3.Next(0, 2);
+            Random random_number_generator_4 = new Random();
+            int random_number_4 = random_number_generator_4.Next(0, 2);
+            Random random_number_generator_5 = new Random();
+            int random_number_5 = random_number_generator_5.Next(0, 2);
+            Random random_number_generator_6 = new Random();
+            int random_number_6 = random_number_generator_6.Next(0, 2);
+            Random random_number_generator_7 = new Random();
+            int random_number_7 = random_number_generator_7.Next(0, 2);
+            Random random_number_generator_8 = new Random();
+            int random_number_8 = random_number_generator_8.Next(0, 2);
+            Random random_number_generator_9 = new Random();
+            int random_number_9 = random_number_generator_9.Next(0, 2);
+            Random random_number_generator_10 = new Random();
+            int random_number_10 = random_number_generator_10.Next(0, 2);
+            Random random_number_generator_11 = new Random();
+            int random_number_11 = random_number_generator_11.Next(0, 2);
+            Random random_number_generator_12 = new Random();
+            int random_number_12 = random_number_generator_12.Next(0, 2);
+            Random random_number_generator_13 = new Random();
+            int random_number_13 = random_number_generator_13.Next(0, 2);
+            Random random_number_generator_14 = new Random();
+            int random_number_14 = random_number_generator_14.Next(0, 2);
+            Random random_number_generator_15 = new Random();
+            int random_number_15 = random_number_generator_15.Next(0, 2);
 
+            //The Ship Intro Speech will always have the scooby laugh. This is how you know the mod is working.
             __instance.shipIntroSpeechSFX = CMSound_Mod_Base.SoundFX[0];
 
+            //The Soocoby Laugh has a random chance to be added to 15 different sounds in the game.
             if (random_number_1 == 1)
             {
                 __instance.playerJumpSFX = CMSound_Mod_Base.SoundFX[0];
--- CMSound_Mod/Plugin.cs
+++ CMSound_Mod/Plugin.cs
@@ -18,7 +18,7 @@ namespace CMSound_Mod
     {
         private const string modGUID = "Poseidon.CMSound_Mod";
         private const string modName = "CM Sound Mod";
-        private const string modVersion = "1.0.0.0";
+        private const string modVersion = "1.1.0.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -26,6 +26,7 @@ namespace CMSound_Mod
 
         internal static ManualLogSource mls;
 
+        //Creates an AudioClip list which is the list that the sound will be heald in.
         internal static List<AudioClip> SoundFX;
         internal static AssetBundle Bundle;
 
@@ -38,6 +39,7 @@ namespace CMSound_Mod
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
+            //This message is printed in the console when the mod has successfully been detected and activated.
             mls.LogInfo("The Scooby Laugh is Active!!!!!");
 
             harmony.PatchAll(typeof(CMSound_Mod_Base));
@@ -45,17 +47,27 @@ namespace CMSound_Mod
 
             mls = Logger;
 
+            //Initializes a new list of AudioClips
             SoundFX = new List<AudioClip>();
+
+            //This insures that the folder it will be checking for sound files is the same folder that the dll file is located in.
             string FolderLocation = Instance.Info.Location;
             FolderLocation = FolderLocation.TrimEnd("CMSound_Mod.dll".ToCharArray());
+
+            //Checks for a file named "scoob" which is the name of the audio file containing the scoob sound effect which is to be built in Unity.
             Bundle = AssetBundle.LoadFromFile(FolderLocation + "scoob");
+
+            //Check if it has loaded correctly into the Bundle variable. If it has, it prints that to the console, letting the user know.
             if (Bundle != null)
             {
                 mls.LogInfo("Successfully loaded asset bundle!");
+                //Appends the audio clip to the list
+                //More audio can be added to this list, and it would be accessed as SoundFX[1], SoundFX[2], and so on.
                 SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
             }
             else
             {
+                //Printed if it fails to load the sound file into the game.
                 mls.LogError("Failed to load asset bundle!");
             }
 
